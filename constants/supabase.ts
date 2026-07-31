8be4d306d3fe9257dfe3ae833e1b9e10c5466fd4
Ana <qananyananahit@gmail.com>
2026-07-31 14:11:30 +0400

    bug fixes again

--- constants/supabase.ts
+++ constants/supabase.ts
@@ -1,8 +1,8 @@
-export const SUPABASE_URL = 'https://ounhrkimkhwlgggbsbkr.supabase.co/rest/v1/';
-export const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+export const SUPABASE_URL = 'https://ounhrkimkhwlgggbsbkr.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91bmhya2lta2h3bGdnZ2JzYmtyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0NzgwNDYsImV4cCI6MjEwMTA1NDA0Nn0.jhxq1sd_S3MoUn_56lqs87MzcIVeeWJQtb_BdQ1DTIw';
 
 export const isSupabaseConfigured =
   !!SUPABASE_URL &&
   !!SUPABASE_ANON_KEY &&
-  SUPABASE_URL !== 'YOUR_SUPABASE_PROJECT_URL' &&
-  SUPABASE_ANON_KEY !== 'YOUR_SUPABASE_ANON_KEY';
+  SUPABASE_URL.startsWith('https://') &&
+  SUPABASE_ANON_KEY.length > 20;
