bf7bbb0dff6cc3c7c5decd012c3e3369e998e94a
Anaaahit <qananyananahit@gmail.com>
2026-07-31 11:33:55 +0400

    api

--- constants/supabase.ts
+++ constants/supabase.ts
@@ -1,4 +1,4 @@
-export const SUPABASE_URL = 'YOUR_SUPABASE_PROJECT_URL';
+export const SUPABASE_URL = 'https://ounhrkimkhwlgggbsbkr.supabase.co/rest/v1/';
 export const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
 
 export const isSupabaseConfigured =
